0f983d85d5541aff11710d274d85e2a615466a61
Kristin Richards <krichards@middlebury.edu>
2016-11-28 22:13:06 -0500

    additions to Parser lib

--- Parser.hs
+++ Parser.hs
@@ -1,22 +1,75 @@
+module Parser where
+
 import Text.ParserCombinators.Parsec
+import qualified Data.List.Split as List
+import Data.List
+-- import Data.Random.Extras
+
+data Option = Help
+            | Choose (Char)
+            | Stats
+            -- | Quit
+      deriving Show
 
 -- :c = chooseEvent
 -- :h = help
 -- :s = stats
--- :p = peek
-options :: Parser Char
-options = do {
-          skipMany space --handle whitespace in beginning of command
+-- :q = Quit
+parseOptions :: Parser Option
+parseOptions = do {
+          spaces --handle whitespace in beginning of command
           ; char ':'
-          ; oneOf "chsp" <* skipMany1 space
+          ; parseChoice <|> parseHelp <|> parseStats
           }
 
--- For testing purposes Use Parsetest
+parseStats :: Parser Option
+parseStats = do {
+            char 's'
+            -- nothing but white space
+            ; return Stats
+            }
+
+parseHelp :: Parser Option
+parseHelp = do {
+            char 'h'
+            ; return Help
+            }
+parseChoice :: Parser Option
+parseChoice = do {
+              char 'c'
+              ; spaces
+              ; choice <- oneOf "abAB"
+              ; return $ Choose choice
+              }
+
+-- parseQuit :: Parser Option
+-- parseQuit = do {
+--             char 'q'
+--             ; return Quit
+--             }
+
+--For testing purposes
 run :: Show a => Parser a -> String -> IO ()
-run p input =
-      case (parse p "" input) of
-        Left err -> do
-                    {putStr "parse error at "
-                    ; print err
-                    }
-        Right x -> print x
+run = parseTest
+
+--------------------------------------------------------------------------------
+madLib :: [String]
+madLib = ["[Creature]", "[Cadj]"]
+
+choices :: [[String]]
+choices = [creatures, cadjs]
+
+ --Look into Vectors
+creatures :: [String]
+creatures = ["kobold", "orc", "bulette", "gelatinous cube"]
+
+cadjs :: [String]
+cadjs =  ["big", "huge", "puney"]
+
+parsePlot :: String -> [String] -> String
+parsePlot s [] = s
+parsePlot s (x:xs) = parsePlot (findAndReplace "test" x s) xs
+
+
+findAndReplace ::  String -> String -> String -> String
+findAndReplace new old s = intercalate new . List.splitOn old $s
